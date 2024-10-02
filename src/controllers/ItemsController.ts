6c976b97e359f1db969c52d934e2b2855a19657d
Alfredo Ortegón Sepúlveda <alorse@gmail.com>
2024-10-01 21:34:15 -0500

    run npx prettier . --write

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -7,11 +7,9 @@ export class ItemsController {
     storageManager: LocalStorageController;
     teams: Team[];
     spaces: Space[];
+    
 
-    constructor(
-        apiWrapper: ApiWrapper,
-        storageManager: LocalStorageController,
-    ) {
+    constructor(apiWrapper: ApiWrapper, storageManager: LocalStorageController) {
         this.apiWrapper = apiWrapper;
         this.storageManager = storageManager;
         this.teams = [];
@@ -39,9 +37,7 @@ export class ItemsController {
     }
 
     async getSpaces(teamId: string): Promise<Space[]> {
-        const storedSpaces: Space[] = await this.storageManager.getValue(
-            `space-${teamId}`,
-        );
+        const storedSpaces: Space[] = await this.storageManager.getValue(`space-${teamId}`);
         if (storedSpaces) {
             this.spaces = storedSpaces;
         } else {
